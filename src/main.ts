dc42942ba41ae5a26db991c5c8708f832826d425
danny-huang-openfind <danny.openfind@gmail.com>
2022-08-08 19:34:26 +0800

    [FIX] Event loop time problem cause by importing node module

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import _ from "lodash-es";
+
 // #region Types
 type Axis = 0 | 1 | -1;
 type Direction = [Axis, Axis];
@@ -5,7 +7,7 @@ type ControllerStorage<ElementType extends HTMLElement> = {
   [prop: string]: ElementType;
 };
 type STATE = "INITIAL" | "PLAYING" | "FINISHED";
-//
+// #endregion
 
 // #region Contstants
 const MILLI_PER_SECOND: Readonly<number> = 1000;
@@ -22,12 +24,11 @@ const PLAYERS: Readonly<string[]> = [BLANK_PLAYER, ..._PLAYERS];
 document.addEventListener("DOMContentLoaded", () => {
   // #region Get Elements' Controller
   const rootElement = document.documentElement;
-  const [actions, gamepad, info] = ["actions", "gamepad", "info"].map((id) =>
+  const [actions, gamepad, info] = _.map(["actions", "gamepad", "info"], (id) =>
     document.getElementById(id)
   ) as [HTMLDivElement, HTMLDivElement, HTMLDivElement];
-  const timer_elements: ControllerStorage<HTMLSpanElement> = PLAYERS.slice(
-    1
-  ).reduce(
+  const timer_elements: ControllerStorage<HTMLSpanElement> = _.reduce(
+    _.slice(PLAYERS, 1),
     (result, player, index) => ({
       ...result,
       [player]: info.children.namedItem("timer")?.children[index]
@@ -35,10 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }),
     {}
   );
-  const buttons: ControllerStorage<HTMLButtonElement> = [
-    "start",
-    "reset",
-  ].reduce(
+  const buttons: ControllerStorage<HTMLButtonElement> = _.reduce(
+    ["start", "reset"],
     (result, name, index) => ({
       ...result,
       [name]: actions.children[index],
@@ -48,10 +47,12 @@ document.addEventListener("DOMContentLoaded", () => {
   // #endregion
 
   // #region Global Variables
-  let timer = PLAYERS.map(() => TOTAL_TIME) as number[];
-  let board = createMappedArray(BOARD_SIZE, () =>
-    Array<string | null>(BOARD_SIZE).fill(null)
+  let timer = _.map(PLAYERS, () => TOTAL_TIME) as number[];
+  let board = _.chunk(
+    _.times<string | null>(BOARD_SIZE ** 2, _.constant(null)),
+    BOARD_SIZE
   );
+
   let current_player_index: number = 0;
   let state: STATE = "INITIAL";
   let ticker: number | null = null;
@@ -59,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // #region Utilities
   function createMappedArray<T>(size: number, mapped_method: () => T): T[] {
-    return [...Array(size)].map(mapped_method);
+    return _.map(_.range(size), mapped_method);
   }
 
   function createElements(tag: string, length: number, classes: string[] = []) {
@@ -75,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function randomPlayer(players_count: number) {
-    return Math.floor(Math.random() * players_count + 1);
+    return _.random(0, players_count - 1) + 1;
   }
   // #endregion
 
@@ -90,10 +91,11 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function resetGame() {
-    board = new Array(BOARD_SIZE)
-      .fill(null)
-      .map(() => new Array<string | null>(BOARD_SIZE).fill(null));
-    for (const box of gamepad.children) {
+    board = _.chunk(
+      _.times<string | null>(BOARD_SIZE ** 2, _.constant(null)),
+      BOARD_SIZE
+    );
+    for (const box of Array.from(gamepad.children)) {
       delete (box as HTMLDivElement).dataset["player"];
     }
 
@@ -104,7 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
     rerender();
 
     for (const player in PLAYERS) {
-      setTimer(+player, TOTAL_TIME);
+      setTimer(_.toNumber(player), TOTAL_TIME);
     }
   }
 
@@ -150,20 +152,23 @@ document.addEventListener("DOMContentLoaded", () => {
       return true;
     }
 
-    let next_position = position.map(
+    let next_position = _.map(
+      position,
       (element, index) => element + (count <= 0 ? -1 : 1) * direction[index]!
     ) as [number, number];
 
-    if (next_position.includes(-1) || next_position.includes(BOARD_SIZE)) {
-      if (count == 0) {
-        next_position = position.map(
+    if (_.some(next_position, (coord) => _.includes([-1, BOARD_SIZE], coord))) {
+      if (_.eq(count, 0)) {
+        next_position = _.map(
+          position,
           (element, index) => element + direction[index]!
         ) as [number, number];
         return trace(next_position, direction, ++count);
       } else if (Math.abs(count) >= BOARD_SIZE - 1) {
         return false;
       } else {
-        next_position = position.map(
+        next_position = _.map(
+          position,
           (element, index) => element + (-count + 1) * direction[index]!
         ) as [number, number];
         return trace(next_position, direction, -count + 1);
@@ -179,15 +184,15 @@ document.addEventListener("DOMContentLoaded", () => {
     player: string
   ) {
     let count = 0;
-    while (count < WIN_LINE_LENGTH) {
-      if (board?.[x]?.[y] !== player) {
+    while (_.inRange(count, WIN_LINE_LENGTH)) {
+      if (!_.isEqual(_.property(`[${x}][${y}]`)(board), player)) {
         return false;
       }
       ++count;
-      [x, y] = [x, y].map((element, index) => element! + direction[index]!) as [
-        number,
-        number
-      ];
+      [x, y] = _.map(
+        [x, y],
+        (element, index) => element! + direction[index]!
+      ) as [number, number];
     }
     return true;
   }
@@ -199,7 +204,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function countDown() {
-    if (state !== "PLAYING" || !timerUpdateAndCheck()) {
+    if (!_.isEqual(state, "PLAYING") || !timerUpdateAndCheck()) {
       return;
     }
 
@@ -212,9 +217,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     requestAnimationFrame(() => {
       setTimeout(() => {
-        const message = player
-          ? `贏家是${PLAYERS[current_player_index]}`
-          : "平手";
+        const message = player ? `贏家是${PLAYERS[player]}` : "平手";
         alert(message);
       });
     });
@@ -225,7 +228,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function rerender() {
     buttons["start"]!.disabled = ["PLAYING", "FINISHED"].includes(state);
-    buttons["reset"]!.disabled = state === "INITIAL";
+    buttons["reset"]!.disabled = _.isEqual(state, "INITIAL");
     rootElement.dataset["state"] = state.toString();
     rootElement.style.setProperty(
       "--player-now",
@@ -238,13 +241,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (ticker) {
       setTimer(
-        +current_player_index,
+        current_player_index,
         timer[current_player_index]! - (now - ticker)
       );
     }
 
-    if (timer[current_player_index] === 0) {
-      declareWinner(getOpponentIndex(current_player_index));
+    if (timer[current_player_index]! <= 0) {
+      _.flow([getOpponentIndex, declareWinner])(current_player_index);
 
       return false;
     }
@@ -255,12 +258,12 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function setTimer(player: number, time: number) {
-    if (!+player || !["O", "X"].includes(PLAYERS[player]!)) {
+    if (!player || !["O", "X"].includes(PLAYERS[player]!)) {
       return;
     }
 
-    const valid_time = Math.max(time, 0);
-    timer[player] = +time;
+    const valid_time = _.max<number>([time, 0])!;
+    timer[player] = time;
     timer_elements[PLAYERS[player]!]!.textContent = (
       valid_time / MILLI_PER_SECOND
     ).toFixed(DECIMAL_DIGITS);
@@ -270,19 +273,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // #region Initialization
   function eventInitialize() {
-    buttons["start"]!.addEventListener("click", startGame);
-    buttons["reset"]!.addEventListener("click", resetGame);
+    _.forOwn(buttons, (button, name) =>
+      button.addEventListener(
+        "click",
+        _.isEqual(name, "start") ? startGame : resetGame
+      )
+    );
 
     gamepad.addEventListener("click", ({ target }) => {
       if (
-        (target as HTMLDivElement).id === "gamepad" ||
+        _.isEqual((target as HTMLDivElement).id, "gamepad") ||
         !timerUpdateAndCheck()
       ) {
         return;
       }
 
       const box = (target as HTMLDivElement).closest("div") as HTMLDivElement;
-      const index = Array.from(box.parentElement?.children!).indexOf(box);
+      const index = _.values(box.parentElement!.children).indexOf(box);
       const [x, y] = [Math.trunc(index / BOARD_SIZE), index % BOARD_SIZE];
 
       setPiece([x, y], current_player_index, box);
@@ -308,10 +315,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (const index in PLAYERS) {
       rootElement.style.setProperty(
-        `--player-${+index == 0 ? "none" : +index - 1}`,
+        `--player-${
+          _.eq(_.toNumber(index), 0) ? "none" : _.toNumber(index) - 1
+        }`,
         `"${PLAYERS[index]}"`
       );
-      setTimer(+index, TOTAL_TIME);
+      setTimer(_.toNumber(index), TOTAL_TIME);
     }
 
     eventInitialize();
