10a2cb673af45a3d4699125f118f933acc8056fa
danny-huang-openfind <danny.openfind@gmail.com>
2022-08-16 11:44:40 +0800

    [NEW] Reset project for new implementation

--- src/types.ts
+++
@@ -1,6 +0,0 @@
-type Axis = 0 | 1 | -1;
-export type Direction = [Axis, Axis];
-export type ControllerStorage<ElementType extends HTMLElement> = {
-  [prop: string]: ElementType;
-};
-export type STATE = "INITIAL" | "PLAYING" | "FINISHED";
